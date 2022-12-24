3f6787aee4ffe62259054165d2f329866118b54b
rising-jun <jun36817@naver.com>
2022-12-24 15:11:26 +0900

    Initial Commit

---
+++ TodayAir/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodayAir
+//
+//  Created by 김동준 on 2022/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodayAir/TodayAirApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayAirApp.swift
+//  TodayAir
+//
+//  Created by 김동준 on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TodayAirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
